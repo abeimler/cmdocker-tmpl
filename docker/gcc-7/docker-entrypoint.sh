9828b27809d8c8a6be4cc143615af5aa8cbb5e4b
Alex Beimler <alex-beimler@web.de>
2017-06-10 07:13:13 +0200

    testing travis ci add more compilers

--- docker/gcc-7/docker-entrypoint.sh
+++ docker/gcc-7/docker-entrypoint.sh
@@ -8,7 +8,7 @@ if [ -f "/toolchain.cmake" ]
 then
     CMAKE_OPTIONS="$CMAKE_OPTIONS -DCMAKE_TOOLCHAIN_FILE:FILEPATH=/toolchain.cmake"
 fi
-CMAKE_OPTIONS="$CMAKE_OPTIONS -DCMAKE_INSTALL_PREFIX:PATH=/prefix"
+CMAKE_OPTIONS="$CMAKE_OPTIONS -DCMAKE_INSTALL_PREFIX:PATH=$PREFIX_PATH"
 
 ./scripts/prebuild.sh
 ./scripts/build.sh
